f758dea526b540753ea4f0ec68401c10690b07d4
Esatollah Ghazi <e.ghazi@student.tugraz.at>
2022-07-07 16:29:24 +0200

    iframe debug true

--- src/Routes/Rooms.jsx
+++ src/Routes/Rooms.jsx
@@ -47,8 +47,8 @@ const Room = () => {
                 </div>
                 <div className={submitted? "flex flex-col justify-center items-center my-4" : "hidden"}>
                     {
-                        submitted ? <iframe width='400' height={window.screen.width < 768 ? '800' : '500'}
-                        allow="microphone;*" title="Jam" src={`https://jam.systems/${rid}?debug=yes#${jamHash}`} /> : null
+                        submitted ? <iframe height={window.screen.width < 768 ? '800' : '500'}
+                        allow="microphone;*" title="Jam" src={`https://jam.systems/${rid}?debug=true#${jamHash}`} /> : null
                     }
                     <div className='text-center w-14 mt-4 mb-1 hover:cursor-pointer bg-transparent hover:bg-red-500 text-red-700 text-sm font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded '
 
